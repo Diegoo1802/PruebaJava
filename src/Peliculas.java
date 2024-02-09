e979d24d50b67d2fefa7b76e1827239c2cd0bdd8
Diego <diegovigara@gmail.com>
2024-02-09 13:13:34 +0100

    Codigo de peliculas acabado

--- src/Peliculas.java
+++ src/Peliculas.java
@@ -1,2 +1,32 @@
 public class Peliculas {
+    private String titulo;
+    private int duracion;
+    /**
+     * Pruebas de comentacion
+     *
+     *
+     *
+     * **/
+    public Peliculas(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
+    /**
+     * Pruebas de comentacion
+     *
+     *
+     *
+     * **/
+    public String getTitulo() {
+        return titulo;
+    }
+    /**
+     * Pruebas de comentacion
+     *
+     *
+     *
+     * **/
+    public int getDuracion() {
+        return duracion;
+    }
 }
